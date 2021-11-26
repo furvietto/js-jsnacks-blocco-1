91020a9428b02b74a1b0e03878d87233ebe12220
furvietto <fulvio-94@hotmail.it>
2021-11-26 13:36:09 +0100

    completing first jsnack

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,5 +1,23 @@
 // Il software deve chiedere per 10 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti.
 
+// con array
+// const container = document.querySelector(".container");
+
+// const arrayNumber = [];
+// let result = 0;
+
+// for (let i = 0; i < 10; i++) {
+
+//     const ask = parseInt(prompt("dimmi un numero"));
+//     arrayNumber.push(ask)
+//     result += arrayNumber[i]
+// }
+
+// container.innerHTML = `il risultato è di ${result}`;
+
+
+
+// senza array
 // const container = document.querySelector(".container");
 
 // let result = 0;
@@ -7,10 +25,13 @@
 // for (let i = 0; i < 10; i++) {
 
 //     const ask = parseInt(prompt("dimmi un numero"));
-//     result += ask ;
+//     result += ask
 // }
 
 // container.innerHTML = `il risultato è di ${result}`;
+
+
+
 
 const container = document.querySelector(".container");
 
